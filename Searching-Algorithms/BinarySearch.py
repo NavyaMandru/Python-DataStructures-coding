5b0f0a8f86a4082f116bef290b82dc775938ab67
Navya Mandru <47963922+NavyaMandru@users.noreply.github.com>
2024-05-06 10:21:29 -0400

    Update BinarySearch.py

--- Searching-Algorithms/BinarySearch.py
+++ Searching-Algorithms/BinarySearch.py
@@ -5,6 +5,42 @@ The idea of binary search is to use the information that the array is sorted and
 
 """
 
+def BinarySearch(arry, x, n):
+
+low = 0
+high = n
+
+while(low <= high):
+  mid = low + (high - low) //2
+
+  if arry[mid] == x:
+    return mid
+  elif arry[mid] < x:
+    low = mid + 1
+  else:
+    high = mid - 1
+
+return -1
 
 if name == "__main__":
+  arru = list()
+  
+  finding_element = int(input("enter the element that you want to find in the list: "))
+  
+  n = int(input("enter no.of elements: "))
+
+  # get the elemets from user and append elements to the list
+  for i in range(n):
+    arry.append(int(input()))
+
+  # sort arry
+  arry = sorted(arry)
+
+  # functtion call
+  result = BinarySearch(arry,finding_element,n)
+
+  if (result == -1):
+    print("Element not present in the list")
+  else:
+    print("Element present the the index ", result)
   
