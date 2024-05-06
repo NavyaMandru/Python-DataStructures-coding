12cdba0313e7d5b81715ac4424f9c93e8ed8b435
Navya Mandru <47963922+NavyaMandru@users.noreply.github.com>
2024-05-06 10:13:05 -0400

    Create BinarySearch.py

---
+++ Searching-Algorithms/BinarySearch.py
@@ -0,0 +1,10 @@
+# Binary Search
+
+"""Binary Search Algorithm is used in a sorted array by repeatedly dividing the search interval in half. 
+The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
+
+"""
+
+
+if name == "__main__":
+  
