4b06d0154d2cd7c717652b9ca742f21160c7d955
Navya Mandru <47963922+NavyaMandru@users.noreply.github.com>
2024-05-06 10:32:52 -0400

    Update BinarySearch.py

--- Searching-Algorithms/BinarySearch.py
+++ Searching-Algorithms/BinarySearch.py
@@ -3,44 +3,54 @@
 """Binary Search Algorithm is used in a sorted array by repeatedly dividing the search interval in half. 
 The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
 
+Time Complexity:
+Best case - O(1)
+Worst Case: O(log N)
+Avarage Case: O(log N)
+
+Space Complexity: O(1)
+
 """
 
-def BinarySearch(arry, x, n):
+def binary_search(arry, x, n):
+    low = 0
+    high = n
 
-low = 0
-high = n
+    while low <= high:
+        mid = low + (high - low) // 2
 
-while(low <= high):
-  mid = low + (high - low) //2
+        # check if x, if present at mid
+        if arry[mid] == x:
+            return mid
+        # If x if greater, ignore left
+        elif arry[mid] < x:
+            low = mid + 1
+        # If x if smaller, ignore right
+        else:
+            high = mid - 1
 
-  if arry[mid] == x:
-    return mid
-  elif arry[mid] < x:
-    low = mid + 1
-  else:
-    high = mid - 1
+    return -1
 
-return -1
 
-if name == "__main__":
-  arru = list()
-  
-  finding_element = int(input("enter the element that you want to find in the list: "))
-  
-  n = int(input("enter no.of elements: "))
+if __name__ == "__main__":
+    arry = list()
 
-  # get the elemets from user and append elements to the list
-  for i in range(n):
-    arry.append(int(input()))
+    finding_element = int(input("enter the element that you want to find in the list: "))
 
-  # sort arry
-  arry = sorted(arry)
+    n = int(input("enter no.of elements: "))
 
-  # functtion call
-  result = BinarySearch(arry,finding_element,n)
+    # get the elements from user and append elements to the list
+    for i in range(n):
+        arry.append(int(input()))
 
-  if (result == -1):
-    print("Element not present in the list")
-  else:
-    print("Element present the the index ", result)
-  
+    # sort arry
+    arry = sorted(arry)
+
+    # function call
+    result = binary_search(arry, finding_element, n)
+
+    if result == -1:
+        print("Element not present in the list")
+    else:
+        print("Element present the the index ", result)
+
